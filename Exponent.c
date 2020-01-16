9da1f5fddf8070c8c33f4bc49db742b844180a08
Tim <timmcmackenjr@gmail.com>
2020-01-15 18:01:37 -0600

    More K&R work

---
+++ Exponent.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+//an example of how to write a function that will take two int and set a to the power of b
+
+int power(int m, int n);
+
+//test the power function
+int main()
+{
+  int i;
+
+  for (i = 0; i < 10; i++)
+    {
+      printf("%d %d %d", i, power(2,i), power(-3,i));
+    }
+  return 0;
+}
+
+//power: raise base to the n-th power; n >= 0
+int power(int base, int n)
+{
+  int i, p;
+
+  p = 1;
+  for(i = 1; i <= n; i++)
+    {
+      p = p * base;
+    }
+  return p;
+}
